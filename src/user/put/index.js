015c939db7886bf92f269ca9b2e7a1e7a2c634f6
Jon Sharratt <jon@craftship.io>
2016-09-11 15:30:13 +0100

    Amend user PUT handler to generate token via github

--- src/user/put/index.js
+++ src/user/put/index.js
@@ -1,45 +1,72 @@
-import AWS from 'aws-sdk'
-import fetch from 'node-fetch'
+import GitHub from 'github'
+import Promise from 'bluebird'
+import env from '../../../env.js'
 
-const loginToNPM = async ({ id, registry, body }) => {
+const loginToGithub = async ({ id, body }, config) => {
   const {
     name,
-    password,
-    email
+    password
   } = JSON.parse(body)
 
-  const reqBody = {
-    _id: id,
-    name,
-    password,
-    email,
-    type: "user",
-    roles: [],
-    date: new Date()
-  }
+  const nameParts = name.split('.')
+  const username = nameParts[0]
+  const otp = nameParts.length > 1 ? nameParts[nameParts.length - 1] : ''
 
-  const req = {
-    method: 'PUT',
+  const github = new GitHub({
+    Promise,
+    protocol: config.github.endpoint.protocol,
+    host: config.github.endpoint.host,
+    pathPrefix: config.github.endpoint.pathPrefix
+  })
+
+  github.authenticate({
+    type: 'basic',
+    username,
+    password
+  })
+
+  let auth = await github.authorization.getOrCreateAuthorizationForApp({
+    scopes: ['user'],
+    client_id: config.github.client_id,
+    client_secret: config.github.secret,
+    note: 'yith private npm registry',
     headers: {
-      'content-type': 'application/json'
-    },
-    body: JSON.stringify(reqBody)
+      'X-GitHub-OTP': otp
+    }
+  })
+
+  if (!auth.token.length) {
+    await github.authorization.delete({
+      id: auth.id,
+      headers: {
+        'X-GitHub-OTP': otp
+      }
+    })
+
+    auth = await github.authorization.create({
+      scopes: ['user'],
+      client_id: config.github.client_id,
+      client_secret: config.github.secret,
+      note: 'yith private npm registry',
+      headers: {
+        'X-GitHub-OTP': otp
+      }
+    })
   }
 
-  try {
-    const response = await fetch(`${registry}-/user/${id}`, req)
-    return await response.json()
-  } catch (error) {
-    return { error: error.message }
-  }
+  return auth
 }
 
-export const handler = async (event, context, callback) => {
-  return callback(null, await (async () => {
-    try {
-      return await loginToNPM(event)
-    } catch (error) {
-      return { error: error.message }
-    }
-  })())
+export const handler = async (event, context) => {
+  const config = env(context)
+
+  try {
+    const { token } = await loginToGithub(event, config)
+    return context.succeed({
+      ok: true,
+      token
+    })
+  } catch (error) {
+    return context.fail(new Error('[401] Unauthorized'))
+  }
 }
